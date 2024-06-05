08f45ba18cb8340e3fdabbba2f281c81310f0cc5
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2024-06-06 01:42:29 +0530

    Connected to Appwrite database

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // express starter template in es6
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import fileUpload from 'express-fileupload';
 import uploadToGithub from './lib/uploadToGithub.js';
+import { ID } from 'node-appwrite';
+import { database } from './lib/appwrite.js';
 
+
+dotenv.config(); // initialize .env
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-dotenv.config(); // initialize .env
 
 
 app.get('/', (req, res) => {
@@ -25,6 +27,8 @@ app.get('/test', (req, res) => {
     res.send('Test route');
 });
 
+
+// pdf upload route
 app.post("/file-upload",
     fileUpload({ createParentPath: true }),
 
@@ -41,11 +45,39 @@ app.post("/file-upload",
         // file.mv(`./uploads/${file.name}`)
 
         const result = await uploadToGithub(file);
+        // const result = github_response_example;
 
         if (result === false) {
             return res.status(500).json({ status: "error", message: "File upload failed" });
         }
 
+        const pdfData = {
+            filename: result.content.name,
+            github_owner: result.owner || "unknown",
+            github_repo: result.repo || "unknown",
+            download_url: result.content.download_url,
+        }
+
+        const databaseId = process.env.APPWRITE_DATABASE_ID;
+        const pdfCollectionId = process.env.APPWRITE_PDF_COLLECTION_ID;
+
+        try {
+
+            const result = await database.createDocument(
+                databaseId,
+                pdfCollectionId,
+                ID.unique(),
+                pdfData
+            );
+
+            console.log("Document created successfully");
+            console.log(result);
+        } catch (error) {
+            console.error("Error creating document");
+            console.error(error);
+
+            return res.status(500).json({ status: "error", message: "Error creating document" });
+        }
 
 
         res.status(200).json({ status: "success", message: "File uploaded successfully" })
